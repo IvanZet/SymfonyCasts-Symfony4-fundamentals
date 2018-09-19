d65c1e04ee4df4b4e0a8c3936a879edf0e1203fb
Ivan Zykov <i.zykov@krone.at>
2018-09-19 18:14:06 +0200

    Adds caching the content of the article about bacons

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Cache\Adapter\AdapterInterface; 
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -23,7 +24,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -32,8 +33,8 @@ class ArticleController extends AbstractController
         ];
 
         $articleContent = <<<EOF
-picy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](http://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha.
-Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](http://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha.
+Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow **turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt venison, et laborum dolore minim non meatball.
 Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon
@@ -44,7 +45,16 @@ buffalo incididunt in filet mignon strip steak pork belly aliquip capicola offic
 adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck fugiat.
 EOF;
 
-        $articleContent = $markdown->transform($articleContent);
+        // Create cache item object in memory to handle cache item
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+
+        if (!$item->isHit()) { // If the key is not already cached
+            // Put the item into cache
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }        
+        // Fetch walue from the cache
+        $articleContent = $item->get();
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
