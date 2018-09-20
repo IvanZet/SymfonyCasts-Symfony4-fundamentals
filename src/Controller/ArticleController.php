21e499e91f86b470fdaa629678800be5ac5223c7
Ivan Zykov <i.zykov@krone.at>
2018-09-20 11:45:02 +0200

    Changed knp_markdown parser to light version, changed caching from default filesystem to APCu

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -37,7 +37,7 @@ Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi 
 Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow **turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt venison, et laborum dolore minim non meatball.
-Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon
+Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim offica. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon
 pork belly ut ullamco. Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
 
 Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit,
