49d98799246a8358aeb1d3e9506588ad674d7fa9
Ivan Zykov <i.zykov@krone.at>
2018-09-20 17:26:45 +0200

    Adds new channel for logging

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface; 
 use Michelf\MarkdownInterface;
 
@@ -9,15 +10,21 @@ class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
     
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
     
     public function parse(string $source): string
     {
+        if(stripos($source, 'bacon')) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+        
         // Create cache item object in memory to handle cache item
         $item = $this->cache->getItem('markdown_'.md5($source));
         
