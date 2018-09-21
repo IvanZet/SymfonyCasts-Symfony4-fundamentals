18d45b3e174047a247c119aab52d7ec97fa74f30
Ivan Zykov <i.zykov@krone.at>
2018-09-21 10:14:14 +0200

    Disables caching in dev and configures it - Disables caching in dev env - Configures method to cache as parameters in services.yaml and services_dev.yaml

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -11,12 +11,14 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    private $isDebug;
     
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
     
     public function parse(string $source): string
@@ -25,6 +27,11 @@ class MarkdownHelper
             $this->logger->info('They are talking about bacon again!');
         }
         
+        // If debuggin on, don't use caching
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+
         // Create cache item object in memory to handle cache item
         $item = $this->cache->getItem('markdown_'.md5($source));
         
